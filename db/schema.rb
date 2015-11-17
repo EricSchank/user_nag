ae2ee58b48252d1651f0a1250885c9ec1b3153f4
Eric Schank <erics@spiceworks.com>
2015-11-16 23:30:11 -0600

    convert to pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151023181114) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "site_configs", force: :cascade do |t|
     t.integer  "site_id",    null: false
     t.string   "nag"
