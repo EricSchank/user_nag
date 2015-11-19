977456a36a9db80c814f031717a4f9856de258b0
Eric Schank <erics@spiceworks.com>
2015-11-18 18:00:34 -0600

    rename to UserNag and add ability to tag just the current ticket

--- app/assets/javascripts/ticket.js
+++ app/assets/javascripts/ticket.js
@@ -1,22 +1,31 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-window.UserNag = {
+window.TicketNag = {
   ticketsNagged: [],
-  nagTicket: function(ticketId){
+  ticketId: undefined,
+  nag: function() {
+    if(TicketNag.ticketId) {
+      TicketNag.nagTicket(TicketNag.ticketId);
+    }
+  },
+  nagTicket: function(ticketId) {
+    var card = new SW.Card();
     card.services('helpdesk').request('comment:create', ticketId, {
       public: true,
       body: 'Just a quick reminder that we need a little more info from you on this issue.\nWhen you get a chance, please give us an update so we can continue to help with this issue.\nThanks!'
     });
   }
 };
-$('document').ready(function(){
+$('document').ready(function() {
   var card = new SW.Card();
-  card.onActivate(function(envData){
-    UserNag.env = envData;
+  card.onActivate(function(envData) {
+    TicketNag.env = envData;
     $('#auid').val(envData['app_host']['auid']);
-    UserNag.site = envData['app_host']['auid'];
+    TicketNag.site = envData['app_host']['auid'];
   });
-  card.services('helpdesk').on('showTicket', UserNag.nagTicket);
-  $('#send_nags').on('click', window.UserNag.nag);
+  card.services('helpdesk').on('showTicket', function(ticketId) {
+    TicketNag.ticketId = ticketId;
+  });
+  $('#send_nags').on('click', window.TicketNag.nag);
 });
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,9 +1,9 @@
 module HomeHelper
   def nag_button(single_ticket=false)
-    poke = 'Poke'
-    poke = poke.pluralize unless single_ticket
+    nag = 'Nag'
+    nag = nag.pluralize unless single_ticket
     button_tag({id: 'send_nags', class: 'waves-effect waves-light btn'}) do
-      content_tag('i', 'email', class: 'material-icons right') + "Send #{poke}"
+      content_tag('i', 'email', class: 'material-icons right') + "Send #{nag}"
     end
   end
 end
