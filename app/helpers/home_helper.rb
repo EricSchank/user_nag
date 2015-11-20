b88e89b960463937aac7d2c16318406a2bb2069b
Eric Schank <erics@spiceworks.com>
2015-11-19 18:24:18 -0600

    simplify and consolidate JS logic

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,9 +1,2 @@
 module HomeHelper
-  def nag_button(single_ticket=false)
-    nag = 'Nag'
-    nag = nag.pluralize unless single_ticket
-    button_tag({id: 'send_nags', class: 'waves-effect waves-light btn'}) do
-      content_tag('i', 'email', class: 'material-icons right') + "Send #{nag}"
-    end
-  end
 end
